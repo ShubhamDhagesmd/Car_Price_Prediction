f753537470ad4980888e7fe103ea31400541bd00
Shubham Dhage <99889956+ShubhamDhagesmd@users.noreply.github.com>
2022-03-12 10:40:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this Vehicle")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Vehicle at {}".format(output))
     else:
         return render_template('index.html')
 
